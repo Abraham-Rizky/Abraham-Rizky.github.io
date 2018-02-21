23c7cda33fcf565d1ba5daac38cb02bc3c82e21e
Rizky Abraham <rizkyabraham88@gmail.com>
2018-02-20 19:59:51 -0800

    edit

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -1,8 +1,15 @@
-function computeWindChill() {
-    var highTemp = get("highTemp").value;
-    var lowTemp = get("lowTemp").value;
-    var windSpeed = get("windSpeed").value;
+function calculateWindChill() {
+    var highTemp = $('#highTemp').val();
+    var lowTemp = $('#lowTemp').val();
+    var windSpeed = $('#windSpeed').val();
     var airAverage = (highTemp + lowTemp) / 2;
     var f = 35.74 + (0.6215 * airAverage) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * airAverage * Math.pow(windSpeed, 0.16));
-    document.getElementById("outputTextBox").value = f;
+
+    document.getElementById("windChill").value = f;
+
+}
+
+function reset() {
+    document.getElementById("windChill").value = "";
+
 }
