b98c015f4270c2d32fdd61f63d6573e0a82795a3
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2018-02-21 15:42:31 -0800

    edit

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -1,7 +1,20 @@
+
+/*function myFunction() {
+    var temp = parseFloat(document.getElementById("highTemp").value);
+    var speed = parseFloat(document.getElementById("windSpeed").value);
+    var result = windChillFunction(temp,speed);
+    document.getElementById("windChill").innerHTML = result;
+}
+
+function windChillFunction(tempF,speed) {
+    var f = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed,0.16)) + (0.4275 * tempF *  Math.pow(speed,0.16)) ;
+    return f;
+}
+*/
 function calculateWindChill() {
-    var highTemp = $('#highTemp').val();
-    var lowTemp = $('#lowTemp').val();
-    var windSpeed = $('#windSpeed').val();
+    var highTemp = parseFloat(document.getElementById(highTemp).value);
+    var lowTemp = parseFloat(document.getElementById(lowTemp).value);
+    var windSpeed = parseFloat(document.getElementById(windSpeed).value);
     var airAverage = (highTemp + lowTemp) / 2;
     var f = 35.74 + (0.6215 * airAverage) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * airAverage * Math.pow(windSpeed, 0.16));
 
@@ -13,3 +26,4 @@ function reset() {
     document.getElementById("windChill").value = "";
 
 }
+
