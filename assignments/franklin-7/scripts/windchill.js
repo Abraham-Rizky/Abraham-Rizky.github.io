2abdfadc1e6cb6c2cc3226a2beafa55c871d97aa
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2018-02-22 10:18:10 -0800

    Edit

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -1,24 +1,12 @@
-
-/*function myFunction() {
-    var temp = parseFloat(document.getElementById("highTemp").value);
-    var speed = parseFloat(document.getElementById("windSpeed").value);
-    var result = windChillFunction(temp,speed);
-    document.getElementById("windChill").innerHTML = result;
-}
-
-function windChillFunction(tempF,speed) {
-    var f = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed,0.16)) + (0.4275 * tempF *  Math.pow(speed,0.16)) ;
-    return f;
-}
-*/
 function calculateWindChill() {
-    var highTemp = 45;
-    var lowTemp = 40;
-    var windSpeed = 25;
+    var highTemp = parseFloat($('body').find('#higTemp').val());
+    var lowTemp = parseFloat($('main').find('#lowTemp').val());
+    var windSpeed = parseFloat($('body').find('#windSpeed').val());
     var airAverage = (highTemp + lowTemp) / 2;
     var f = 35.74 + (0.6215 * airAverage) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * airAverage * Math.pow(windSpeed, 0.16));
+    var fRound = Number.parseFloat(f).toFixed(2);
 
-    document.getElementById("windChill").value = f;
+    document.getElementById("windChill").value = fRound;
 
 }
 
