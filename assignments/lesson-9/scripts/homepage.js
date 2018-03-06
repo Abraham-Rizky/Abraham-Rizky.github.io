88b2e02f7a037d8aff1d6b6e6108430df6ccefc5
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2018-03-05 20:42:35 -0800

    Edited

--- assignments/lesson-9/scripts/homepage.js
+++ assignments/lesson-9/scripts/homepage.js
@@ -2,13 +2,11 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
-
-
-            var motto = document.getElementById("franklin-json").innerHTML = "Motto : " + myObj.towns[0].motto;
-            var year = document.getElementById("franklin-json").innerHTML = "Year founded : " + myObj.towns[0].yearFounded;
-            var population = document.getElementById("franklin-json").innerHTML = "Population : " + myObj.towns[0].currentPopulation;
-            var rainfall = document.getElementById("franklin-json").innerHTML = "Annual rainfall : " + myObj.towns[0].averageRainfall + "  inches";
-
+        var newParagraph = document.createElement('h3');
+        var motto = $('#franklin-json').text("Motto : " + myObj.towns[0].motto );
+        var motto = $('#franklin-json').append("\n", " Year founded : " + myObj.towns[0].yearFounded);
+        var motto = $('#franklin-json').append( " Population : " + myObj.towns[0].currentPopulation);
+        var motto = $('#franklin-json').append( " Annual rainfall : " + myObj.towns[0].averageRainfall + "  inches");
     }
 };
 xmlhttp.open("GET", "  https://byui-cit230.github.io/weather/data/towndata.json", true);
