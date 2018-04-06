3ca87f754782d609bc3b2a9cdf88539cf0049e94
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2018-04-06 07:31:05 -0700

    edit

--- assignments/term-project/scripts/pricing.js
+++ assignments/term-project/scripts/pricing.js
@@ -1,15 +1,11 @@
-/*var xmlhttp = new XMLHttpRequest();
+var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        var myObj = JSON.parse(this.responseText), gold = $('#gold').text(myObj.rizkydata[0].gold);
 
-
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        var gold = $('#gold').text(myObj.rizkydata[0].gold);
     }
+
 };
 xmlhttp.open("GET", "https://abraham-rizky.github.io/assignments/term-project/data/pricing.json", true);
 xmlhttp.send();
-*/
-$.get("https://abraham-rizky.github.io/assignments/term-project/data/pricing.json", function (data) {
-    $("#gold")
-        .append("Name: " + data.rizkydata[0].gold);
-});
