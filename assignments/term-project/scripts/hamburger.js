d6c5a24579f392b54dd09ea08de900a43db3beba
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2018-04-07 16:35:37 -0700

    json

--- assignments/term-project/scripts/hamburger.js
+++ assignments/term-project/scripts/hamburger.js
@@ -41,17 +41,3 @@ window.onload = function () {
 
 }
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-
-        var gold = $('#gold').text(myObj.towns[0].gold);
-        var silver = $('#silver').text(myObj.towns[0].silver);
-        var bronze = $('#bronze').text(myObj.towns[0].bronze);
-        var drivetrain = $('#drivetrain').text(myObj.towns[1].drivetrain);
-
-    }
-};
-xmlhttp.open("GET", "http://abraham-rizky.github.io/assignments/term-project/data/pricing.json", true);
-xmlhttp.send();
---
+++ assignments/term-project/scripts/pricing.js
@@ -0,0 +1,14 @@
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+
+        var gold = $('#gold').text(myObj.towns[0].gold);
+        var silver = $('#silver').text(myObj.towns[0].silver);
+        var bronze = $('#bronze').text(myObj.towns[0].bronze);
+        var drivetrain = $('#drivetrain').text(myObj.towns[1].drivetrain);
+
+    }
+};
+xmlhttp.open("GET", "http://abraham-rizky.github.io/assignments/term-project/data/pricing.json", true);
+xmlhttp.send();
