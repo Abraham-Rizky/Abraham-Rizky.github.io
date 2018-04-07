0dff1c5f06352d787a03c9f3de50bda0d8a318c4
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2018-04-07 16:23:27 -0700

    Edit json file

--- assignments/term-project/scripts/pricing.js
+++ assignments/term-project/scripts/pricing.js
@@ -3,9 +3,9 @@ xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
 
-        var gold = $('#gold').text(myObj.towns[0].name);
+        var gold = $('#gold').text(myObj.towns[0].yearFounded);
 
     }
 };
-xmlhttp.open("GET", "https://abraham-rizky.github.io/assignments/term-project/data/pricing.json", true);
+xmlhttp.open("GET", "http://abraham-rizky.github.io/assignments/term-project/data/pricing.json", true);
 xmlhttp.send();
