0a9864aa66943008937808782eb2e40b179dfb7a
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2018-04-07 16:29:29 -0700

    Json

--- assignments/term-project/scripts/hamburger.js
+++ assignments/term-project/scripts/hamburger.js
@@ -40,3 +40,17 @@ window.onload = function () {
     })();
 
 }
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+
+        var gold = $('#gold').text(myObj.towns[0].gold);
+        var silver = $('#silver').text(myObj.towns[0].silver);
+        var silver = $('#bronze').text(myObj.towns[0].bronze);
+
+    }
+};
+xmlhttp.open("GET", "http://abraham-rizky.github.io/assignments/term-project/data/pricing.json", true);
+xmlhttp.send();
