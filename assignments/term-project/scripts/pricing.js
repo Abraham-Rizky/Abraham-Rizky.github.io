d29b115b91e7c7bd500cfb8ca283e96f5eef34fb
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2018-04-07 16:13:20 -0700

    Edit

--- assignments/term-project/scripts/pricing.js
+++ assignments/term-project/scripts/pricing.js
@@ -1,11 +1,11 @@
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
-
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
-        var gold = $('#gold').text(myObj.rizkydata[0].gold);
-    }
 
+        var gold = $('#gold').text(myObj.towns[0].gold );
+
+    }
 };
-xmlhttp.open("GET", "https://abraham-rizky.github.io/assignments/term-project/data/pricing.json", true);
+xmlhttp.open("GET", "https://byui-cit230.github.io/weather/data/towndata.json", true);
 xmlhttp.send();
