bc0251b506428c1f0c8e409fe8cd96befc7be39c
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2018-04-11 05:36:00 -0700

    Add wayfinding

--- assignments/term-project/scripts/hamburger.js
+++ assignments/term-project/scripts/hamburger.js
@@ -37,3 +37,19 @@ window.onload = function () {
     })();
 
 }
+
+var urlString, urlArray, pageHREF, menu, i, currentURL;
+urlString = document.location.href;
+urlArray = urlString.split('/');
+pageHREF = urlArray[urlArray.length - 1];
+
+if (pageHREF !== "") {
+    menu = document.querySelectorAll('ul#mainmenu li a');
+    for (i = 0; i < menu.length; i++) {
+        currentURL = (menu[i].getAttribute('href'));
+        menu[i].className = '';
+        if (currentURL === pageHREF) {
+            menu[i].className = 'active';
+        }
+    }
+}
